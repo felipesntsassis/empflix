14a2a3b74584a43c174f014c7349cfa6eeae7cc2
Felipe dos Santos Assis <felipesntsassis@gmail.com>
2020-07-29 07:26:19 -0300

    * Implementação do efeito de escala das imagens. * Personalização dos dados. * Personalização do logotipo.

--- src/components/Carousel/components/VideoCard/styles.js
+++ src/components/Carousel/components/VideoCard/styles.js
@@ -1,31 +1,34 @@
 import styled from "styled-components";
 
 export const VideoCardContainer = styled.a`
-	border: 2px solid;
-	border-radius: 4px;
-	text-decoration: none;
-	overflow: hidden;
-	cursor: pointer;
-	color: white;
-	flex: 0 0 298px;
-	width: 298px;
-	height: 197px;
-	background-image: ${({ url }) => `url(${url})`};
-	background-size: cover;
-	background-position: center;
-	border-radius: 10px;
-	position: relative;
-	display: flex;
-	align-items: flex-end;
-	padding: 16px;
+			border: 2px solid;
+			border-radius: 4px;
+			text-decoration: none;
+			overflow: hidden;
+			cursor: pointer;
+			color: white;
+			flex: 0 0 298px;
+			width: 298px;
+			height: 197px;
+			background-image: ${({ url }) => `url(${url})`};
+			background-size: cover;
+			background-position: center;
+			border-radius: 10px;
+			position: relative;
+			display: flex;
+			align-items: flex-end;
+			padding: 16px;
+            opacity: 0.5;
+            transition: all 0.3s ease-in-out;
+            
+			&:hover,
+			&:focus {
+				opacity: 1;
+                transform: scale(1.2);
+                z-index: 2;
+			}
 
-	transition: opacity 0.3s;
-	&:hover,
-	&:focus {
-		opacity: 0.5;
-	}
-
-	&:not(:first-child) {
-		margin-left: 20px;
-	}
-`;
+			&:not(:first-child) {
+				margin-left: 20px;
+			}
+		`;
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,18 +3,29 @@ import { FooterBase } from './styles';
 
 function Footer() {
   return (
-    <FooterBase>
-      <a href="https://www.alura.com.br/">
-        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
-      </a>
-      <p>
-        Orgulhosamente criado durante a
-        {' '}
-        <a href="https://www.alura.com.br/">
-          Imersão React da Alura
-        </a>
-      </p>
-    </FooterBase>
+		<FooterBase>
+			<a href="https://www.alura.com.br/">
+				<img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+			</a>
+			<p>
+				<strong>Empflix</strong>. Sua TV digital sobre empreendedorismo criado com{" "}
+				<a href="https://reactjs.org/" target="_blank">ReactJS</a>.
+                <br/>
+				Desenvolvido por{" "}
+				<a href="https://www.linkedin.com/in/felipesntsassis/" target="_blank">
+					Felipe Assis
+				</a>
+                <br/>
+				Código-fonte disponível em{" "}
+				<a href="https://github.com/felipesntsassis/empflix" target="_blank">
+					felipesntsassis/empflix
+				</a>
+				.
+			</p>
+			<p>
+				Orgulhosamente criado durante a <a href="https://www.alura.com.br/" target="_blank">Imersão React da Alura</a>
+			</p>
+		</FooterBase>
   );
 }
 
