dd50b475c0a81a2a1b75dfc76f4c043a8923dd23
Felipe dos Santos Assis <felipesntsassis@gmail.com>
2020-07-31 22:00:40 -0300

    Ajustes no componente YouTubeIframeResponsive

--- src/components/BannerMain/components/VideoIframeResponsive/index.js
+++ src/components/BannerMain/components/VideoIframeResponsive/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-YouTubeIframeResponsiveYouTubeIframeResponsiveimport PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 import { VideoContainer, ResponsiveIframe } from './styles';
 
